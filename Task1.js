f020c792e2b9757dafe270805b29b5d7b24680f2
Naitik1611 <shah.naitik.v@gmail.com>
2023-10-16 16:04:15 +0530

    second commit

--- Task1.js
+++ Task1.js
@@ -1,7 +1,7 @@
-API_URL=""
+API_URL="https://raw.githubusercontent.com/Naitik1611/battles/main/battles.json"
 async function getData(){
     try {
-        const response = await fetch("");
+        const response = await fetch(API_URL);
         const data = await response.json();
         console.log('Fetched data:');
         console.log(JSON.stringify(data, null, 2));
